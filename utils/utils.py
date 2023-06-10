5271cc9b7fb0dbaa63207d9c76441d867a3ca161
Балахонцева Анастасия <ayamileto@gmail.com>
2023-06-10 07:08:41 +0300

    changed load_data func

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,14 @@
 import json
 
 
-def load_data():
+def load_data(filename):
     """ Функция открывает и читает файл """
-    with open('data', 'r', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
+
+
+
+
 
 
