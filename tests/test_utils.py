86a97a1b9ffc01bd56dddd6c541d9c8bec56cd11
Балахонцева Анастасия <ayamileto@gmail.com>
2023-06-10 07:10:21 +0300

    add fixture and test for loud_data func

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,22 @@
 import pytest
-from utils import utils
+from utils.utils import load_data
+
+
+@pytest.fixture()
+def testing_data():
+    temp_data = "test_data_file.json"
+    return temp_data
+
+
+def test_load_data(testing_data):
+    """ Тест на корректность загрузки файла """
+    loaded_data = load_data(testing_data)
+    assert loaded_data == [
+        {"id": 1, "name": "Mary", "state": "EXECUTED"},
+        {"id": 2, "name": "Jerry", "state": "CANCELED"},
+        {},
+        {"id": 3, "name": "Piter", "state": "EXECUTED"}
+    ]
+
+
 
