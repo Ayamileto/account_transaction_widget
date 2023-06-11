d7a14c8c6e17c5c21ad0c4d89975680c2084e2ff
Балахонцева Анастасия <ayamileto@gmail.com>
2023-06-11 06:57:02 +0300

    add tests for get_filtered_data func

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,11 @@
+import os
 import pytest
-from utils.utils import load_data
+from utils.utils import load_data, get_filtered_data
 
 
 @pytest.fixture()
 def testing_data():
-    temp_data = "test_data_file.json"
+    temp_data = os.path.join(os.path.dirname(__file__), "test_data_file.json")
     return temp_data
 
 
@@ -19,4 +20,31 @@ def test_load_data(testing_data):
     ]
 
 
+@pytest.mark.parametrize('testing_data, expected', [
+    ([
+        {"id": 1, "name": "Mary", "state": "EXECUTED"},
+        {"id": 2, "name": "Jerry", "state": "CANCELED"},
+        {},
+        {"id": 3, "name": "Piter", "state": "EXECUTED"}
+        ],
+        [
+        {"id": 1, "name": "Mary", "state": "EXECUTED"},
+        {"id": 3, "name": "Piter", "state": "EXECUTED"}
+    ])
+])
+def test_get_filtered_data(testing_data, expected):
+    """ Тест на фильтрацию данных """
+    filtered_data = get_filtered_data(testing_data)
+    assert filtered_data == expected
+
+
+@pytest.mark.parametrize('testing_data, expected', [
+    ([
+        {"id": 1, "name": "Mary", "state": "EXECUTED"},
+        {"id": 2, "name": "Jerry"}],
+        KeyError)
+])
+def test_get_filtered_dataKeyError(testing_data, expected):
+    with pytest.raises(expected):
+        get_filtered_data(testing_data)
 
